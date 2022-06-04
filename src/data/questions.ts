d580470fa04d048a9e05f1dc0e51189aca1827b6
TeluTrix <lucaschbern@hotmail.com>
2022-06-04 11:11:27 +0200

    Added more ABU questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -4,11 +4,123 @@ export const questions = [
         "answer": "47 Staaten"
     },
     {
-        "question": "Wie viele Staaten umfasst die Europäische Union?",
+        "question": "Wie viele Staaten umfasst die Europäische Union (EU)?",
         "answer": "27 Staaten"
     },
     {
-        "question": "Wie viele Staaten umfasst der Nordantlantikpakt?",
+        "question": "Wie viele Staaten umfasst der Nordantlantikpakt (NATO)?",
         "answer": "26 Staaten"
-    }
+    },
+    {
+        "question": "Mit welcher anderen Organisation arbeit die EU zusammen?",
+        "answer": "OSZE"
+    },
+    {
+        "question": "In welchen Organisationen ist die Schweiz Mitglied?",
+        "answer": "Europarat, OSZE, UNO"
+    },
+    {
+        "question": "Was ist der Sinn des Nordatlanikpakts (NATO)?",
+        "answer": "Militärisches Bündnis - Für Freiheit und Demokratie"
+    },
+    {
+        "question": "Wie viele Staaten umfasst die OSZE?",
+        "answer": "56 Staaten"
+    },
+    {
+        "question": "Für was steht die Abkürzung OSZE?",
+        "answer": "Organisation für Sicherheit und Zusammenarbeit in Europa"
+    },
+    {
+        "question": "Wie viele Staaten umfasst die UNO?",
+        "answer": "192 Staaten"
+    },
+    {
+        "question": "Was ist der Zweck der UNO?",
+        "answer": "Die UNO ist zuständig für die weltweite Wahrung des Friedens."
+    },
+    {
+        "question": "Wann wurde der Europarat gegründet?",
+        "answer": "1949"
+    },
+    {
+        "question": "Nenne die wichtigsten Ziele des Europarats",
+        "answer": "Achtung der Menschenrechte, Wahrung von Grundfreiheiten, Bessere Lebensbedingungen bei Mitgliedern"
+    },
+    {
+        "question": "Was ist eine Konvention?",
+        "answer": "internationale Übereinkommen / Abmachung"
+    },
+    {
+        "question": "Für was steht EMRK?",
+        "answer": "Die Europäische Menschenrechtskonvention"
+    },
+    {
+        "question": "Was beinhaltet die Europäische Menschenrechtskonvention(EMRK)?",
+        "answer": "Rechte auf: Leben, Freiheit, Meinungsäusserung, freie Wahlen, sich friedlich zu versammeln, angemessenes Verhör"
+    },
+    {
+        "question": "Was ist die Sozialcharta?",
+        "answer": "Garantie für Sozialrechte"
+    },
+    {
+        "question": "Was für Sozialrechte beinhaltet die Sozialcharta?",
+        "answer": "Recht auf: sichere & gesunde Arbeitsbedingungen, Fürsorge, Berufsausbildung, Arbeit, Kinder, soziale Sicherheit"
+    },
+    {
+        "question": "Welche 4 Organe gibt es im Europarat?",
+        "answer": "Ministerkomitee, Parlamentarische Versammlung, Kongress der Gemeinden und Regionen Europas, Europäischer Gerichtshof für Menschenrechte in Strassburg"
+    },
+    {
+        "question": "Wofür ist das Ministerkomitee?",
+        "answer": "Mit am meisten Macht, können Empfehlungen von der Parlamentarischen Versammlung umsetzen"
+    },
+    {
+        "question": "Wie viele Minister hat es im Ministerkomitee?",
+        "answer": "1x pro Land / 47 Minister"
+    },
+    {
+        "question": "Welches vertritt die Schweiz im Ministerkomitee?",
+        "answer": "Ignazio Cassis (Aussenminister der Schweiz)"
+    },
+    {
+        "question": "Wie viele male treffen sich die Minister im Ministerkomitee pro Jahr?",
+        "answer": "1/Jahr"
+    },
+    {
+        "question": "Wofür ist die Parlamentarische Versammlung?",
+        "answer": "Um Empfehlungen zu erarbeiten die nachher an das Ministerkomitee weitergegeben."
+    },
+    {
+        "question": "Wie viele Mitglieder hat die Parlamentarische Versammlung?",
+        "answer": "318 Mitglieder + 318 Stellvertreter"
+    },
+    {
+        "question": "Wie viele Sitzungen hat der Kongress der Gemeinden und Regionen Europas?",
+        "answer": "1 Jährlich"
+    },
+    {
+        "question": "Wie viele Vetreter hat die Schweiz im Kongress der Gemeinden und Regionen Europas?",
+        "answer": "6 Vertreter + 6 Stellvertreter"
+    },
+    {
+        "question": "Was ist der Zweck des Kongress der Gemeinden und Regionen Europas?",
+        "answer": "Beraten mit Ministerkomitee, Fördert die Schaffung von Kantionaler Regierungstrukturen"
+    },
+    {
+        "question": "Wie häufig tagt der Europäische Gerichtshof für Menschenrechte in Strasburg?",
+        "answer": "Durchgehend"
+    },
+    {
+        "question": "Wie viele Mitglieder hat der Europäische Gerichtshof für Menschhenrechte in Strasburg?",
+        "answer": "1 Richter pro Land / 47 Richter"
+    },
+    {
+        "question": "Was ist der Zweck des Europäischen Gerichtshof für Menschenrechte in Strasburg?",
+        "answer": "Beurteilen von Verstössen gegen die Europäische Menschenrechtskonvention (Good to Know: Die Richter vetreten zwar ihr Land, sind jedoch unabhängig)."
+    },
+    {
+        "question": "In welchem Jahr ist die Schweiz dem Europarat beigetreten?",
+        "answer": "1963"
+    },
 ]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <div class="w-full text-center">
+    <div class="w-full">
       <div href="#" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Learn ABU - Europäische Sicherheitsarchitektur</h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">You can do this!</p>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Learn ABU - Europäische Sicherheitsarchitektur</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 text-center">Practicing card {{ counter + 1 }}/{{ cardAmount }}</p>
         <div class="flex justify-center">
-          <div v-if="clicked !== true && done !== true" href="#" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2" @click="turnCard">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8">{{ currentQuestion }}</h5>
+          <div v-if="clicked !== true && done !== true" class="block h-56 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2" @click="turnCard">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8 align-middle">{{ currentQuestion }}</h5>
           </div>
-          <div v-if="clicked === true && done !== true" href="#" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8">{{ currentAnswer }}</h5>
-            <button @click="goToNext" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Next</button>
+          <div v-if="clicked === true && done !== true" class="block h-56 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2" @click="turnCard">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8 align-middle">{{ currentAnswer }}</h5>
           </div>
-          <div v-if="done === true" href="#" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2 text-center">
+          <div v-if="done === true" class="block h-56 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-8 w-1/2 text-center">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8">You are done!</h5>
-            <div>
-              <img class="w-24 rounded" src="beer.png" />
-            </div>
+          </div>
+        </div>
+        <div class="flex justify-center mt-4">
+          <div>
+            <button @click="goOneBack" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Back</button>
+          </div>
+          <div>
+            <button @click="clearCards" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Clear</button>
+          </div>
+          <div>
+            <button @click="goToNext" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Next</button>
           </div>
         </div>
       </div>
@@ -41,20 +48,44 @@ export default {
   },
   methods: {
     turnCard(){
-      this.clicked = true;
+      if(this.clicked === true){
+        this.clicked = false;
+      }else{
+        this.clicked = true;
+      }
     },
     goToNext(){
       this.clicked = false;
-
-      this.currentQuestion = questions[this.counter].question;
-      this.currentAnswer = questions[this.counter].answer;
-
       this.counter += 1;
+
+      if(questions[this.counter] !== undefined){
+        this.currentQuestion = questions[this.counter].question;
+        this.currentAnswer = questions[this.counter].answer;
+      }
+    },
+    goOneBack(){
+      this.clicked = false;
+      this.counter -= 1;
+
+      if(questions[this.counter] !== undefined){
+        this.currentQuestion = questions[this.counter].question;
+        this.currentAnswer = questions[this.counter].answer;
+      }
+    },
+    clearCards(){
+      this.done = false;
+      this.clicked = false;
+      this.counter = 0;
+
+      if(questions[this.counter] !== undefined){
+        this.currentQuestion = questions[this.counter].question;
+        this.currentAnswer = questions[this.counter].answer;
+      }
     }
   },
   watch: {
     'counter'(newValue) {
-      if(newValue === (this.cardAmount + 1)){
+      if(newValue === (this.cardAmount)){
         this.done = true;
       }
     }
@@ -62,7 +93,6 @@ export default {
   beforeMount() {
     this.currentQuestion = questions[this.counter].question;
     this.currentAnswer = questions[this.counter].answer;
-    this.counter += 1;
     this.cardAmount = questions.length;
   }
 }
